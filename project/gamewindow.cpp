7819cb0a44e86426d3a2b750078ac8f8f5768caa
Nickel-Crumple <warlen94@mail.ru>
2014-04-24 16:33:14 +0600

    First Step

--- project/gamewindow.cpp
+++ project/gamewindow.cpp
@@ -6,9 +6,25 @@ gameWindow::gameWindow(QWidget *parent) :
     ui(new Ui::gameWindow)
 {
     ui->setupUi(this);
+
+    gameScene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(gameScene);
+
+    QBrush redBrush(Qt::red);
+
+    QPen blackpen(Qt::black);
+    blackpen.setWidth(2);
+
+    ball = gameScene->addEllipse(10,10,13,13,blackpen,redBrush);
+    ball->setFlag(QGraphicsItem::ItemIsMovable);
 }
 
 gameWindow::~gameWindow()
 {
     delete ui;
+}
+
+void gameWindow::on_actionExit_triggered()
+{
+    qApp->exit();
 }
--- project/gamewindow.h
+++ project/gamewindow.h
@@ -1,6 +1,9 @@
 #ifndef GAMEWINDOW_H
 #define GAMEWINDOW_H
 
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
 #include <QMainWindow>
 
 namespace Ui {
@@ -15,8 +18,13 @@ public:
     explicit gameWindow(QWidget *parent = 0);
     ~gameWindow();
 
+private slots:
+    void on_actionExit_triggered();
+
 private:
     Ui::gameWindow *ui;
+    QGraphicsScene *gameScene;
+    QGraphicsEllipseItem *ball;
 };
 
 #endif // GAMEWINDOW_H
